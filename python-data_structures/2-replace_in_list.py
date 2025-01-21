e6dd9da63edb65622947718c0a4d6c4a16b67d3a
Your Name <you@example.com>
2025-01-21 15:11:49 +0100

    Python - Data Structures: Lists, Tuples

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,7 +1,17 @@
-def replace_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
-        return my_list
-    else:
+#!/usr/bin/python3
+my_list = [1, 2, 3, 4, 5]
+idx = 3
+new_element = 9
+print("Original list:", my_list)
+new_list = replace_in_list(my_list, idx, new_element)
+print("New list:", new_list)
 
-        my_list[idx] = element
-        return my_list
+# Testing invalid index
+idx = 5
+new_list = replace_in_list(my_list, idx, new_element)
+print("List after invalid index:", new_list)
+
+# Testing negative index
+idx = -1
+new_list = replace_in_list(my_list, idx, new_element)
+print("List after negative index:", new_list)
