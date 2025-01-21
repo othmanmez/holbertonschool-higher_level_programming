821e784eeefc2ab6f4d213004341fe0fe686164d
Your Name <you@example.com>
2025-01-21 14:05:20 +0100

    Python - Data Structures: Lists, Tuples

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,8 +2,10 @@
 
 import marshal
 
+
 # Fonction pour extraire et afficher les noms depuis le fichier .pyc
 def print_names_from_pyc(pyc_file_path):
+    
     # Ouvrir le fichier .pyc
     with open(pyc_file_path, 'rb') as f:
         # Le fichier .pyc commence par un en-tête de 16 octets qu'on ignore
@@ -22,12 +24,4 @@ def print_names_from_pyc(pyc_file_path):
 
     # Afficher chaque nom valide sur une nouvelle ligne
     for name in valid_names:
-        print(name)
-
-# Si ce script est exécuté directement (et non importé), alors...
-if __name__ == "__main__":
-    # Définir le chemin du fichier .pyc téléchargé
-    pyc_file_path = '/tmp/hidden_4.pyc'
-
-    # Appeler la fonction pour afficher les noms
-    print_names_from_pyc(pyc_file_path)
+        print(
