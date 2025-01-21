184a5dc897d66dc4e67198f1482203ac2c55c606
Your Name <you@example.com>
2025-01-21 15:32:14 +0100

    8-m

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,12 +2,16 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     # Gestion du tuple a
     if len(tuple_a) == 0:
+        
         tuple_a = (0, 0)
     elif len(tuple_a) == 1:
+
         tuple_a = (tuple_a[0], 0)
 
     # Gestion du tuple b
+
     if len(tuple_b) == 0:
+
         tuple_b = (0, 0)
     elif len(tuple_b) == 1:
         tuple_b = (tuple_b[0], 0)
