4e24bd04cd2c313a0af1e3288d2f53bc736605f2
Your Name <you@example.com>
2025-01-21 16:44:10 +0100

    Python - Data Structures: Lists, Tuples

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 
-def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx >= len(my_list):
-        return my_list
-    return my_list[:idx] + my_list[idx+1:]
 
+def delete_at(my_list=[], idx=0):
+    if my_list is None:
+        return None
+    if idx < 0:
+        return my_list
+    if idx >= len(my_list):
+        return my_list
+    del my_list[idx]
+    return my_list
