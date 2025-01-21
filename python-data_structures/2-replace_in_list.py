0ed933ebce7356e9880bc033a1215f1c9df9a6ec
Your Name <you@example.com>
2025-01-21 14:28:21 +0100

    Python - Data Structures: Lists, Tuples

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,5 +1,8 @@
 def replace_in_list(my_list, idx, element):
+    # Vérifier si l'index est valide
     if idx < 0 or idx >= len(my_list):
         return my_list
-    my_list[idx] = element
-    return my_list
+    else:
+        # Remplacer l'élément à la position spécifiée
+        my_list[idx] = element
+        return my_list
