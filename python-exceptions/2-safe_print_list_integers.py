e1f351e475cae5cb2e4335162e719798d39bc7c8
Your Name <you@example.com>
2025-01-22 15:34:23 +0100

    Python - Exceptions

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    count = 0
+    p = 0
     for i in range(x):
         try:
-            print("{d}".format(my_list=[], x=0))
-            count + 1
-            execpt(ValueErrorError, TypeError)
+            print("{:d}".format(my_list[i]), end="")
+            p += 1
+        except (ValueError, TypeError):
             continue
-
-
-print()
-
-
-return count
+    print("")
+    return p
