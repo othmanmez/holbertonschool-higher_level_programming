15c99d0e70783498b1154e7a780f1498a7ff1787
Your Name <you@example.com>
2025-01-22 15:12:38 +0100

    Python - Exceptions

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -7,4 +7,6 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[i]), end="")
             p += 1
         except (ValueError, TypeError):
-            return False
+            continue
+    print("")
+    return p
