ec71804560ce41ab08e36ccbd4eb9c3735a47a0f
Your Name <you@example.com>
2025-01-22 15:11:28 +0100

    Python - Exceptions

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
-def safe_print_integer(value):
-    try:
-        print("{d}.formmat: (value))
-        return True
-        except (ValueError, TypeERROR):
+def safe_print_list_integers(my_list=[], x=0):
+    p = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            p += 1
+        except (ValueError, TypeError):
             return False
