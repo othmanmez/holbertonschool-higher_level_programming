02127714fada092371fe213d8003c593c2abd4a9
Your Name <you@example.com>
2025-01-22 15:22:40 +0100

    Python - Exceptions

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        print("{d}".format(my_list=[], x=0))
+        return True
+    execpt(my_listError, TypeError)
+    return False
