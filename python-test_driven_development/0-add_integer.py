f0a2ef468f2865cf25a790e04bc303e525e571de
Your Name <you@example.com>
2025-01-24 14:15:40 +0100

    Python - Test-driven development

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python3
+"""
+Module qui définit une fonction pour additionner deux entiers.
+"""
+
 def add_integer(a, b=98):
+    """
+    Fonction qui additionne deux entiers (ou flottants).
+    
+    Paramètres:
+    - a: Le premier nombre (entier ou flottant).
+    - b: Le second nombre (entier ou flottant, par défaut 98).
+    
+    Retourne:
+    La somme de a et b en tant qu'entier.
+    """
+    # Vérifier que 'a' est un entier ou un flottant
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
+    
+    # Vérifier que 'b' est un entier ou un flottant
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-
-    a = int(a)
-    b = int(b)
-
-    return a + b
+    
+    # Retourner la somme de a et b après les avoir convertis en entiers
+    return int(a) + int(b)
