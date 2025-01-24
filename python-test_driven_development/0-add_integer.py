5a211ed2a205bf4b491c7970064a4a2886c76788
Your Name <you@example.com>
2025-01-24 14:10:36 +0100

    Python - Test-driven development

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -9,4 +9,3 @@ def add_integer(a, b=98):
     b = int(b)
 
     return a + b
-
--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+
 def matrix_divided(matrix, div):
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not isinstance(matrix, list) or not all(
+            isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of int/float")
 
     if not all(isinstance(el, (int, float)) for row in matrix for el in row):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix (listoflists) of ints/floats")
 
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
@@ -16,4 +18,3 @@ def matrix_divided(matrix, div):
         raise ZeroDivisionError("division by zero")
 
     return [[round(el / div, 2) for el in row] for row in matrix]
-
