72f5e0ee12988080ffc880d667c5dbeb15177914
Your Name <you@example.com>
2025-01-26 02:19:48 +0100

    Python - Test-driven development

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
+""" print a text """
+
 
 def text_indentation(text):
-    if not isinstance(text, str):
+    """ print a text """
+    if type(text) is not str:
         raise TypeError("text must be a string")
+    x = 0
+    for i in text:
+        if x == 0:
+            if i == ' ':
+                continue
+            x = 1
 
-    characters = ['.', '?', ':']
-    start = 0
-
-    while start < len(text):
-        if text[start] in characters:
-            print(text[start].strip())
-            print()
-            start += 1
-        elif text[start] == " ":
-            start += 1
-        else:
-            print(text[start], end="")
-            start += 1
+        if x == 1:
+            if i in ".?:":
+                print(i + '\n')
+                x = 0
+            else:
+                print(i, end='')
