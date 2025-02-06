167fc1bc732197f268c756710324b5b77985f00a
Your Name <you@example.com>
2025-02-06 14:49:33 +0100

    Python - Inheritance

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""Définition de la classe MyList qui hérite de list."""
+"""class MyList that inherits from list"""
 
 
 class MyList(list):
-    """Classe qui hérite de list et ajoute une méthode d'affichage trié."""
-
-
-def print_sorted(self):
-    """Affiche la liste triée en ordre croissant."""
-    print(sorted(self))  # Utilise sorted() pour trier sans modifier l'original
+    """inherits from list"""
+    def print_sorted(self):
+        """print the sorted list"""
+        print(sorted(self))
