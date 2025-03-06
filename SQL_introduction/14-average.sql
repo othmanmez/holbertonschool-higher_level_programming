d244354fec6fdfab067adc5bb5b67b0e2fd6d7c2
Your Name <you@example.com>
2025-03-06 14:07:18 +0100

    SQL - Introduction

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,4 +1,2 @@
-Write a script that computes the score average of all records in the table second_table of the database hbtn_0c_0 in your MySQL server.
-
-The result column name should be average
-The database name will be passed as an argument of the mysql command
+-- Write a script that computes the score average of all records in the table second_table
+SELECT AVG(score) 'average'  FROM second_table;
--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,2 @@
--- Afficher la structure de la table first_table en interrogeant la table information_schema
-SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH 
-FROM information_schema.columns 
-WHERE table_schema = '$1' AND table_name = 'first_table';
-
+-- script that prints the following description of the table first_table from the database hbtn_0c_0
+SHOW CREATE TABLE first_table;
